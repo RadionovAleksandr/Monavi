c7d53df8165ddfe5058a3d037ce842fa8c4fedf8
Radionov Alexandr <Radionovalek@yandex.ru>
2019-05-31 12:57:35 +0800

    claen my js code

--- js/movavi-2.js
+++ js/movavi-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function() {
-    var basket = [];
+    var basket = []; //масив отображаемый в корзине
     var cardData = [{
             id: 0,
             cardImg: "img/коробка1.jpg",
@@ -25,48 +25,6 @@
         }
     ];
 
-
-    //Ф-ия  отрисовывает элементы корзины ссылаясь на элементы коллекции "basket"
-    var createBasketItem = function() {
-        var basketList = document.querySelector('.basketList');
-        while (basketList.firstChild) {
-            basketList.removeChild(basketList.firstChild)
-        };
-        basket.forEach(function(basketItem, index) {
-            // for (i = 0; i < basket.length; i++) {
-            var basketItem = makeElement('div', 'basket__item');
-            basketList.appendChild(basketItem);
-            var itemImg = makeElement('img', 'basket__item-img');
-            itemImg.src = 'img/крестик.png';
-            itemImg.alt = 'Кнопка закрыть';
-            basketItem.appendChild(itemImg);
-            var itemText = makeElement('div', 'basket__item-text', basket[index].cardTitle);
-            basketItem.appendChild(itemText);
-            var itemPrice = makeElement('div', 'basket__item-price', basket[index].cardPrice);
-            basketItem.appendChild(itemPrice);
-            basketItem.querySelector('.basket__item-img').dataset.id = index;
-        })
-    }
-
-    //ф-ия получает из localStorage данные
-    var getToStorage = function() {
-        var returnBasket = JSON.parse(localStorage.getItem("products")) //спарсим его обратно объект
-        if (returnBasket) {
-            basket = returnBasket;
-            createBasketItem()
-            basketSum(basket)
-        } else {
-            console.log('LocalStorage пустой');
-        }
-    }
-
-    var saveToStorage = function() {
-        var serialBasket = JSON.stringify(basket); //сериализуем 
-        localStorage.setItem("products", serialBasket); //запишем в хранилище по ключу "products"
-    }
-
-
-
     // ф-ия создает элементы для вставки в DOM
     var makeElement = function(tagName, className, text) {
         var element = document.createElement(tagName);
@@ -77,9 +35,14 @@
         return element;
     };
 
-    // Предварительно проверив localStorage создаю карточки каталога
-    getToStorage();
-    //ф-ия создания карточек каталога
+    //ф-ия отрисовывает обновленные элементы из массива "basket" и навешивание слушателя на обновленные элементы basket."закрыть"
+    var addToCard = function(evt) {
+        var id = evt.target.dataset.id;
+        basket.push(cardData[id]); //добавляет элементы в коллекцию корзины "basket"
+        updatCart(); //обновление корзины
+    };
+
+    //ф-ия создает карточки каталога, и навешивает слушатель на кнопку "Вкорзину"
     cardData.forEach(function(cardDataItem, index) {
         var ListElement = document.querySelector('.goods');
         var cardContainer = makeElement('div', 'goods__item');
@@ -104,56 +67,79 @@
         var button = makeElement('button', 'goods__button', 'В корзину!');
         button.classList.add('button');
         buttonWrap.appendChild(button);
-        cardContainer.querySelector('.goods__button').dataset.id = index;
+        button.addEventListener('click', addToCard); //Слушатель на кнопку "В корзину"
+        cardContainer.querySelector('.goods__button').dataset.id = index; // задаем индекс элементу
     });
 
+    //ф-ия вешает слушатель на кнопку "закрыть"
+    var onlistenerClose = function() {
+        var btnClose = document.querySelectorAll('.basket__item-img');
+        btnClose.forEach(function(btnCloseItem) {
+            if (btnClose) {
+                btnCloseItem.addEventListener('click', onButtonClose);
+            }
+        })
+    };
 
-    //ф-ия отрисовки обновленных элементов в коллекции "basket" и навешивание слушателя на обновленные элементы basket."закрыть"
-    var onButtonClick = function(evt) {
-        var id = evt.target.dataset.id;
-        basket.push(cardData[id]); //добавляет элементы в коллекцию корзины "basket"
-        createBasketItem();
+    //Ф-ия обновляет состояние корзины и отрисовывает элементы корзины ссылаясь на элементы обновленного массива "basket"
+    var updatCart = function() {
+        var basketList = document.querySelector('.basketList');
+        while (basketList.firstChild) {
+            basketList.removeChild(basketList.firstChild)
+        }
+        basket.forEach(function(basketItem, index) {
+            console.log(basketItem.cardTitle);
+            console.log(basket[index].cardTitle); // Есть вопрос для изучения
+            console.log(index);
+            var basketItem = makeElement('div', 'basket__item');
+            basketList.appendChild(basketItem);
+            var itemImg = makeElement('img', 'basket__item-img');
+            itemImg.src = 'img/крестик.png';
+            itemImg.alt = 'Кнопка закрыть';
+            // itemImg.addEventListener('click', onButtonClose);
+            basketItem.appendChild(itemImg);
+            var itemText = makeElement('div', 'basket__item-text', basket[index].cardTitle);
+            basketItem.appendChild(itemText);
+            var itemPrice = makeElement('div', 'basket__item-price', basket[index].cardPrice);
+            basketItem.appendChild(itemPrice);
+            basketItem.querySelector('.basket__item-img').dataset.id = index;
+        })
         saveToStorage(); //запишем в localStorage обновленный список 
+        onlistenerClose(); // вешаю слушатели на обновленные кнопки "Закрыть" элементов корзины
         basketSum(basket); //обновляем общую сумму
-        onlistenerClose();
-
     };
 
-    //Слушатель на кнопку "В корзину"
-    var buttons = document.querySelectorAll('.goods__button');
-    for (var i = 0; i < buttons.length; i++) {
-        if (buttons) {
-            buttons[i].addEventListener('click', onButtonClick);
-        };
+    //ф-ия сохраняет в localStorage данные
+    var saveToStorage = function() {
+        var serialBasket = JSON.stringify(basket); //сериализуем 
+        localStorage.setItem("products", serialBasket); //запишем в хранилище по ключу "products"
     };
+
+    //ф-ия получает из localStorage данные
+    var getToStorage = function() {
+        var returnBasket = JSON.parse(localStorage.getItem("products")) //спарсим его обратно объект
+        if (returnBasket) {
+            basket = returnBasket;
+            updatCart()
+        } else {
+            console.log('LocalStorage пустой');
+        }
+    };
+
+    //  проверяем localStorage, если есть элементы отрисовываем в "basket"
+    getToStorage();
 
     //ф-ия удаляет элемент из коллекции "basket" и отрисовывает обновленную коллекцию в элементы корзины
     var onButtonClose = function(evt) {
         var btnClose = document.querySelectorAll('.basket__item-img');
         var id = evt.target.dataset.id;
-        for (i = 0; i < btnClose.length; i++) {
-            if (btnClose[i].dataset.id === id) {
+        btnClose.forEach(function(btnCloseItem) {
+            if (btnCloseItem.dataset.id === id) {
                 basket.splice(id, 1); // удаляю  элемент коллекции
-                saveToStorage(); //запишем в localStorage обновленный список 
-                getToStorage();
-                createBasketItem();
-                basketSum(basket);
+                updatCart();
             }
-        }
-        //вешаю слушатель на обновленные элементы коллекции basket."закрыть"
-        onlistenerClose()
-    }
-
-    //ф-ия вешает слушатель на кнопку "закрыть"
-    var onlistenerClose = function() {
-        var btnClose = document.querySelectorAll('.basket__item-img');
-
-        for (i = 0; i < btnClose.length; i++) {
-            if (btnClose) {
-                btnClose[i].addEventListener('click', onButtonClose);
-            }
-        };
-    }
+        })
+    };
 
     onlistenerClose() //вешаю слушатель на элементы коллекции basket."закрыть"
 
@@ -161,10 +147,10 @@
     function basketSum(array) {
         var basketSum = document.querySelector('.basket__summ-number')
         var sum = 0;
-        for (var i = 0; i < array.length; i++) {
-            sum += array[i].cardPrice;
-        }
+        array.forEach(function(arrayItem) {
+            sum += arrayItem.cardPrice;
+        })
         basketSum.textContent = sum;
         return (basketSum)
-    }
+    };
 })();
